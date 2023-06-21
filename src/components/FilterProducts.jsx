e65160ef62c23c72d3169ef76aaf701456629e1b
allan lenny <owinoallan77@gmail.com>
2023-06-22 01:04:04 +0300

    filtered products

--- src/components/FilterProducts.jsx
+++ src/components/FilterProducts.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
 import { ImFilter } from 'react-icons/im'
+import { GiRunningShoe, GiArmoredPants } from 'react-icons/gi'
+import {FaHeadphones, FaTshirt} from 'react-icons/fa'
 import './pages/home.css'
 
-const FilterProducts = ({ category, onClick,isActive }) => {
+const FilterProducts = ({ category, onClick, isActive }) => {
+ 
+    if (category === "shoes") {
+      return (
+        <div className="available-products" onClick={onClick}>
+          <div className={isActive ? "isActive" : "knife"}>
+            <GiRunningShoe />
+          </div>
+          <p>{category}</p>
+        </div>
+      )
+  }
+  
+    if (category === "trousers") {
+      return (
+        <div className="available-products" onClick={onClick}>
+          <div className={isActive ? "isActive" : "knife"}>
+            <GiArmoredPants/>
+          </div>
+          <p>{category}</p>
+        </div>
+      )
+  }
+  
+    if (category === "electronics") {
+      return (
+        <div className="available-products" onClick={onClick}>
+          <div className={isActive ? "isActive" : "knife"}>
+            <FaHeadphones />
+          </div>
+          <p>{category}</p>
+        </div>
+      )
+  }
+  
+    if (category === "shirts") {
+      return (
+        <div className="available-products" onClick={onClick}>
+          <div className={isActive ? "isActive" : "knife"}>
+            <FaTshirt />
+          </div>
+          <p>{category}</p>
+        </div>
+      )
+    }
+  
   return (
     <div>
       <div className="available-products" onClick={onClick}>
