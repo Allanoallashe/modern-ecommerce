9204862ac499f1b42802846f42401d9ad440f0ef
allan lenny <owinoallan77@gmail.com>
2023-08-03 13:11:35 +0300

    flying buttons

--- src/components/cardFeatures.jsx
+++ src/components/cardFeatures.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './pages/home.css'
 import { BsFillCartPlusFill } from 'react-icons/bs'
 import {IoIosPricetags} from 'react-icons/io'
 import { Link } from 'react-router-dom'
 import { addCartItem } from '../redux/productSlice'
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import anime from 'animejs'
 
 
 const CardFeatures = ({ image, name, price, category, loading, id}) => {
   
+  
+
   const dispatch = useDispatch()
-  const [isAdded, setIsAdded] = useState(false)
+  
   const handleAddTocart = (e) => {
     dispatch(addCartItem({
       _id: id,
@@ -20,38 +22,81 @@ const CardFeatures = ({ image, name, price, category, loading, id}) => {
       price: price,
       category: category,
     }))
-    setIsAdded(true)
-
-    // animate products
-    anime({
-      targets: '.card-container',
-      duration: 5000,
-      easing: "easeInOutQuad",
-      opacity: {
-        from: 0,
-        to: 1,
-      },
-      scale: {
-        from: 0.5,
-        to: 1,
-      },
-      rotate: '1turn',
-      translateY: (500).offSetTop,
-    })
+    
+  
   }
 
+  
+  const [isAnimated, setIsAnimated] = useState(false)
+  const itemRef = useRef(null)
+  const copyRef = useRef(null)
+
+
+  const flyProduct = () => {
+    const item = itemRef.current
+    const itemCopy = item.cloneNode(true)
+
+    const boundingRect = item.getBoundingClientRect()
+
+    itemCopy.style.position = 'fixed';
+    itemCopy.style.left = `${boundingRect.left}px`;
+    itemCopy.style.top = `${boundingRect.top}px`;
+    itemCopy.style.opacity = 1
+    document.body.appendChild(itemCopy);
+    const targetX = window.innerWidth - item.offsetWidth - 20
+    const targetY = -20
+    setIsAnimated(true)
+      anime({
+        targets: itemCopy,
+        translateX: targetX - boundingRect.left,
+        translateY: targetY - boundingRect.top,
+        opacity: 0,
+        duration: 2000,
+        easing: 'easeOutQuad',
+        complete: () => {
+          document.body.removeChild(itemCopy);
+          setIsAnimated(false)
+        },
+      })
+    
+  }
+  
+ 
+
+
   return (
-    <div className="card-container">
+    <div className="card-container"
+    >
       {image ?
-        <>
-          <Link to={`/Menu/${id}`} onClick={() =>window.scrollTo({top:"0", behavior:"smooth"})} >
-            <img src={image} alt="cartImage" />
+        <><div style={{position:'relative',}}>
+          <Link to={`/Menu/${id}`} onClick={() => window.scrollTo({ top: "0", behavior: "smooth" })}
+          style={{position:'relative',}}
+          >
+            {isAnimated && (
+              <img
+                ref={copyRef}
+                style={{
+                  position: 'absolute',
+                  zIndex: 500,
+                }}
+                src={image} alt="cartImage" />)}
+              
+              <img ref={itemRef} src={image} alt="cartImage" />
+            
           </Link>
+          </div>
             <p>{name}</p>
             <p><IoIosPricetags/> {price}</p>
             <p>{category}</p>
             <div className="button">
-              <button onClick={handleAddTocart}><a>Add to Cart <BsFillCartPlusFill/></a></button>
+            <button onClick={() => {
+              handleAddTocart();
+              flyProduct()
+            }}>
+              <a>
+                  Add to Cart <BsFillCartPlusFill />
+              </a>
+            </button>
             </div>
         </>
         :
